d76f732a31448a6d729466d945b55c0368f686b7
sandrotiiago <sandrotiiago@gmail.com>
2023-12-07 10:43:03 +0000

    Fix QL-810WC printer model

--- src/android/BasePrint.java
+++ src/android/BasePrint.java
@@ -183,7 +183,8 @@ public abstract class BasePrint {
                 case QL_720NW:
                 case QL_800:
                 case QL_810W:
-                case QL_810WC:
+                case QL_810Wc(70, QL_580N):
+                case QL_810WC(71, QL_580N):
                 case QL_820NWB:
                     mPrinterInfo.labelNameIndex = LabelInfo.QL700.valueOf(
                             sharedPreferences.getString("paperSize", LabelInfo.QL700.W62.toString())).ordinal();
@@ -675,7 +676,8 @@ public abstract class BasePrint {
             case PT_P900W:
             case PT_P950NW:
             case QL_810W:
-            case QL_810WC:
+            case case QL_810Wc(70, QL_580N):
+            case case QL_810WC(71, QL_580N):
             case QL_800:
             case QL_820NWB:
             case PT_P300BT:
