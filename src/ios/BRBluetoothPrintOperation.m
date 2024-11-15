d7e97e7eaddb940e17dce00e9addde760214b396
Micael <a.manueljoao.aftec@gmail.com>
2024-11-15 12:02:01 +0100

    fix blutehoot

--- src/ios/BRBluetoothPrintOperation.m
+++ src/ios/BRBluetoothPrintOperation.m
@@ -50,18 +50,18 @@
     // Démarrer la communication avec l'imprimante
     if ([self.ptp startCommunication]) {
         self.communicationResultForBT = YES;
-        
+
         // Récupérer le statut de l'imprimante
         BRPtouchPrinterStatus *status = [[BRPtouchPrinterStatus alloc] init];
         int error = [self.ptp getStatus:&status];
-        
-        if (error == ERROR_NONE_ && status.statusInfo.byErrorInf == 0 && status.statusInfo.byErrorInf2 == 0) {
+
+        if ((error == ERROR_NONE_ || error == 1)  && status.statusInfo.byErrorInf == 0 && status.statusInfo.byErrorInf2 == 0) {
             // Aucune erreur détectée, l'imprimante est prête
             [self.ptp setPrintInfo:self.printInfo];
 
             // Démarrer l'impression de l'image
             int printResult = [self.ptp printImage:self.imgRef copy:self.numberOfPaper];
-            if (printResult == ERROR_NONE_) {
+            if (printResult == ERROR_NONE_ || error == 1) {
                 PTSTATUSINFO resultstatus;
                 [self.ptp getPTStatus:&resultstatus];
                 self.resultStatus = resultstatus;
@@ -70,7 +70,7 @@
             NSLog(@"Erreur d'imprimante détectée : byErrorInf = %d, byErrorInf2 = %d", status.statusInfo.byErrorInf, status.statusInfo.byErrorInf2);
             self.communicationResultForBT = NO;
         }
-        
+
         [self.ptp endCommunication];
     } else {
         self.communicationResultForBT = NO;
